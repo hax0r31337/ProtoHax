b569d7270dc70afb1829aae47d590cb52aafcbc3
Coft Anery <AneryCoft13363134145@gmail.com>
2023-01-07 19:20:15 +0800

    控制击退强度

--- src/main/java/dev/sora/relay/cheat/module/impl/ModuleVelocity.kt
+++ src/main/java/dev/sora/relay/cheat/module/impl/ModuleVelocity.kt
@@ -4,13 +4,17 @@ import com.nukkitx.protocol.bedrock.packet.SetEntityMotionPacket
 import dev.sora.relay.cheat.module.CheatModule
 import dev.sora.relay.game.event.Listen
 import dev.sora.relay.game.event.impl.EventPacketInbound
+import dev.sora.relay.cheat.value.FloatValue
 
 class ModuleVelocity : CheatModule("Velocity") {
+    private val horizontalValue = FloatValue("Horizontal", 0f, 0f, 1f)
+    private val verticalValue = FloatValue("Vertical", 0f, 0f, 1f)
 
     @Listen
     fun onPacketInbound(event: EventPacketInbound) {
         if (event.packet is SetEntityMotionPacket) {
-            event.cancel()
+            event.packet.motion.mul(horizontalValue.get(),verticalValue.get(),horizontalValue.get())
+            //event.cancel()
         }
     }
 }
