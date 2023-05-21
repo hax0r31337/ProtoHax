37080c83ab60a8930c15cbff40d4a68498a8d10b
hax0r31337 <liulihaocaiqwq@gmail.com>
2023-05-21 01:32:36 +0000

    feat: AntiBlind fire

--- src/main/java/dev/sora/relay/cheat/module/impl/ModuleAntiBlind.kt
+++ src/main/java/dev/sora/relay/cheat/module/impl/ModuleAntiBlind.kt
@@ -4,11 +4,14 @@ import dev.sora.relay.cheat.module.CheatModule
 import dev.sora.relay.game.event.EventPacketInbound
 import dev.sora.relay.game.event.EventTick
 import dev.sora.relay.game.utils.constants.Effect
+import org.cloudburstmc.protocol.bedrock.data.entity.EntityFlag
 import org.cloudburstmc.protocol.bedrock.packet.MobEffectPacket
+import org.cloudburstmc.protocol.bedrock.packet.SetEntityDataPacket
 
 class ModuleAntiBlind : CheatModule("AntiBlind") {
 
     private var nightVisionValue by boolValue("NightVision", true)
+	private var removeFireValue by boolValue("RemoveFire", false)
     private var removeBadEffectsValue by boolValue("RemoveBadEffects", true)
 
 	override fun onDisable() {
@@ -34,11 +37,15 @@ class ModuleAntiBlind : CheatModule("AntiBlind") {
 		})
 	}
 
-	private val handlePacketInbound = handle<EventPacketInbound>(this::removeBadEffectsValue) { event ->
+	private val handlePacketInbound = handle<EventPacketInbound> { event ->
 		val packet = event.packet
-		if (packet is MobEffectPacket) {
+		if (removeBadEffectsValue && packet is MobEffectPacket) {
 			if (packet.effectId == Effect.NAUSEA || packet.effectId == Effect.BLINDNESS || packet.effectId == Effect.DARKNESS) {
 				event.cancel()
+			}
+		} else if (removeFireValue && packet is SetEntityDataPacket && packet.runtimeEntityId == event.session.thePlayer.runtimeEntityId) {
+			if (packet.metadata.flags.contains(EntityFlag.ON_FIRE)) {
+				packet.metadata.setFlag(EntityFlag.ON_FIRE, false)
 			}
 		}
 	}
