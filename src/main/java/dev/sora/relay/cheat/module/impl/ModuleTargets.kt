d11b2c4779fb18d3c171241311cc3263544160f5
hax0r31337 <liulihaocaiqwq@gmail.com>
2023-05-31 02:03:46 +0000

    feat: team check

--- src/main/java/dev/sora/relay/cheat/module/impl/ModuleTargets.kt
+++ src/main/java/dev/sora/relay/cheat/module/impl/ModuleTargets.kt
@@ -6,16 +6,19 @@ import dev.sora.relay.game.entity.Entity
 import dev.sora.relay.game.entity.EntityPlayer
 import dev.sora.relay.game.entity.EntityPlayerSP
 import dev.sora.relay.game.entity.EntityUnknown
+import dev.sora.relay.utils.analyzeColorCoverage
+import org.cloudburstmc.protocol.bedrock.data.entity.EntityDataTypes
 
 class ModuleTargets : CheatModule("Targets", canToggle = false) {
 
 	private var targetPlayersValue by boolValue("TargetPlayers", true)
 	private var targetEntitiesValue by boolValue("TargetEntities", false)
     private var antiBotModeValue by listValue("AntiBotMode", AntiBotMode.values(), AntiBotMode.NONE)
+	private var teamCheckModeValue by listValue("TeamCheckMode", TeamCheckMode.values(), TeamCheckMode.NONE)
 
 	fun Entity.isTarget(): Boolean {
 		return if (this == session.thePlayer) false
-		else if (targetPlayersValue && this is EntityPlayer) !this.isBot()
+		else if (targetPlayersValue && this is EntityPlayer) !this.isBot() && !this.isTeammate()
 		else if (targetEntitiesValue && this is EntityUnknown) true
 		else false
 	}
@@ -32,8 +35,27 @@ class ModuleTargets : CheatModule("Targets", canToggle = false) {
         }
     }
 
+	fun EntityPlayer.isTeammate(): Boolean {
+		if (this is EntityPlayerSP || !state) return false
+
+		return when (teamCheckModeValue) {
+			TeamCheckMode.NAME_TAG -> {
+				val selfColor = session.thePlayer.metadata[EntityDataTypes.NAME]?.let { analyzeColorCoverage(it as String).maxBy { it.value }.key } ?: 'f'
+				val playerColor = this.metadata[EntityDataTypes.NAME]?.let { analyzeColorCoverage(it as String).maxBy { it.value }.key } ?: 'f'
+
+				selfColor == playerColor
+			}
+			TeamCheckMode.NONE -> false
+		}
+	}
+
 	private enum class AntiBotMode(override val choiceName: String) : NamedChoice {
         PLAYER_LIST("PlayerList"),
 		NONE("None")
     }
+
+	private enum class TeamCheckMode(override val choiceName: String) : NamedChoice {
+		NAME_TAG("NameTag"),
+		NONE("None")
+	}
 }
--- src/main/java/dev/sora/relay/utils/StringUtils.kt
+++ src/main/java/dev/sora/relay/utils/StringUtils.kt
@@ -16,3 +16,33 @@ fun ByteArray.toHexString(separator: String = ""): String {
 fun base64Decode(b64: String): ByteArray {
     return Base64.getDecoder().decode(b64.replace('-', '+').replace('_', '/'))
 }
+
+private val controlCodesMinecraftString = arrayOf('k', 'l', 'm', 'n', 'o', 'r')
+
+/**
+ * counts color coverage of a Minecraft colored (§) string
+ */
+fun analyzeColorCoverage(str: String): Map<Char, Int> {
+	val stringParts = str.split('§')
+	val colorMap = mutableMapOf<Char, Int>()
+
+	var currentColor = 'f'
+
+	stringParts.forEachIndexed { i, s ->
+		val p = if (i == 0) {
+			s
+		} else {
+			val control = s[0]
+			if (!controlCodesMinecraftString.contains(control)) {
+				currentColor = control
+			} else if (control == 'r') { // resets color
+				currentColor = 'f'
+			}
+			s.substring(1)
+		}
+
+		colorMap[currentColor] = (colorMap[currentColor] ?: 0) + p.length
+	}
+
+	return colorMap
+}
