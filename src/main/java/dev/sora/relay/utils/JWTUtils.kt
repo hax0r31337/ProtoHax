14f00d75455de0183c50390b0d57416ad1b7cdff
hax0r31337 <liulihaocaiqwq@gmail.com>
2023-06-28 13:53:59 +0000

    chore: improve jwt signing

--- src/main/java/dev/sora/relay/utils/JWTUtils.kt
+++ src/main/java/dev/sora/relay/utils/JWTUtils.kt
@@ -3,6 +3,7 @@ package dev.sora.relay.utils
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import dev.sora.relay.cheat.config.AbstractConfigManager
+import org.jose4j.jws.EcdsaUsingShaAlgorithm
 import java.security.KeyPair
 import java.security.Signature
 import java.security.SignatureException
@@ -36,51 +37,6 @@ private fun signBytes(dataToSign: ByteArray, keyPair: KeyPair): String {
 	val signature = Signature.getInstance("SHA384withECDSA")
 	signature.initSign(keyPair.private)
 	signature.update(dataToSign)
-	val signatureBytes = derToJose(signature.sign())
+	val signatureBytes = EcdsaUsingShaAlgorithm.convertDerToConcatenated(signature.sign(), 48)
 	return Base64.getUrlEncoder().withoutPadding().encodeToString(signatureBytes)
-}
-
-private fun derToJose(derSignature: ByteArray): ByteArray {
-	val ecNumberSize = 48
-	val derEncoded = derSignature[0] == 0x30.toByte() && derSignature.size != ecNumberSize * 2
-
-	if (!derEncoded) {
-		throw SignatureException("Invalid DER signature format.")
-	}
-
-	val joseSignature = ByteArray(ecNumberSize * 2)
-
-	var offset = 1
-	if (derSignature[1] == 0x81.toByte()) {
-		offset++
-	}
-
-	val encodedLength = derSignature[offset++].toInt() and 0xff
-	if (encodedLength != derSignature.size - offset) {
-		throw SignatureException("Invalid DER signature format.")
-	}
-
-	offset++
-
-	val rLength = derSignature[offset++].toInt()
-	if (rLength > ecNumberSize + 1) {
-		throw SignatureException("Invalid DER signature format.")
-	}
-	val rPadding = ecNumberSize - rLength
-	val rStart = offset + max(-rPadding, 0)
-	val rEnd = rLength + min(rPadding, 0)
-	derSignature.copyInto(joseSignature, max(rPadding, 0), rStart, rStart + rEnd)
-
-	offset += rLength + 1
-
-	val sLength = derSignature[offset++].toInt()
-	if (sLength > ecNumberSize + 1) {
-		throw SignatureException("Invalid DER signature format.")
-	}
-	val sPadding = ecNumberSize - sLength
-	val sStart = offset + max(-sPadding, 0)
-	val sEnd = sLength + min(sPadding, 0)
-	derSignature.copyInto(joseSignature, ecNumberSize + max(sPadding, 0), sStart, sStart + sEnd)
-
-	return joseSignature
 }
